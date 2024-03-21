f3a6b8dc68209ac84e85b6a61d6172b1dcac07b0
Carlosguilherme95 <carlos_buske@hotmail.com>
2024-03-21 10:51:08 -0300

    testes

--- Main.java
+++ Main.java
@@ -1,8 +1,31 @@
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.assertTrue;
+
 public class Main {
+    @Test
+    public void testeUm() {
+        List<String> pessoas = new ArrayList<>();
+        pessoas.add("Carlos-M");
+        pessoas.add("Vitor-M");
+        pessoas.add("Gustavo-M");
+        pessoas.add("Luana-F");
+        pessoas.add("Marcia-F");
+        pessoas.add("Rose-F");
+
+        List<String> mulheres = pessoas.stream().filter(p -> p.endsWith("-F")).toList();
+
+        // Verificar se todas as pessoas na lista "mulheres" terminam com "-F"
+        assertTrue("A lista de mulheres não pode estar vazia", !mulheres.isEmpty());
+        for (String mulher : mulheres) {
+            assertTrue("O nome da mulher não termina com '-F'", mulher.endsWith("-F"));
+        }
+    }
+
     public static void main(String[] args) {
         List<String> pessoas = new ArrayList<>();
         pessoas.add("Carlos-M");
@@ -14,5 +37,6 @@ public class Main {
         System.out.println(pessoas);
         List<String> mulheres = pessoas.stream().filter(p -> p.endsWith("-F")).toList();
         System.out.println(mulheres);
+       
     }
 }
